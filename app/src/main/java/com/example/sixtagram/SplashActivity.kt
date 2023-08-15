650e8d80fada3b716a1c816b0e4316f84386abde
joye-seo <104261048+joye-seo@users.noreply.github.com>
2023-08-15 09:54:07 +0900

    [conflict] 충돌 해결

--- app/src/main/java/com/example/sixtagram/SplashActivity.kt
+++ app/src/main/java/com/example/sixtagram/SplashActivity.kt
@@ -1,11 +1,24 @@
 package com.example.sixtagram
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.os.postDelayed
 
 class SplashActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
+        actionSplash()
+    }
+
+    private fun actionSplash() {
+        Handler().postDelayed(2500) {
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
     }
 }
