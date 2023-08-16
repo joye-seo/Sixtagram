2442e4d708cf946f17c26532fc17d73d83914fa1
asdsad86642 <asdsad86642@gmail.com>
2023-08-16 09:35:13 +0900

    [개발]화면 뼈대 작업!

--- app/src/main/java/com/example/sixtagram/calander/CalanderActivity.kt
+++ app/src/main/java/com/example/sixtagram/calander/CalanderActivity.kt
@@ -2,12 +2,26 @@ package com.example.sixtagram.calander
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.RecyclerView
 import com.example.sixtagram.R
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class CalanderActivity : AppCompatActivity() {
+
+    lateinit var mRvDiary : RecyclerView
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
+
         setContentView(R.layout.activity_calander)
+
+        mRvDiary = findViewById(R.id.recycler_diary)
+
+        val btnBlueWrite : FloatingActionButton = findViewById(R.id.btn_blue_write)
+        btnBlueWrite.setOnClickListener {
+
+
+
+        }
     }
 }
-//커밋 잘되는지 테스트
