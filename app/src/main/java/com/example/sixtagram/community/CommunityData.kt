9f05d5064c0a49133c46ba01d22ee11f819112f4
joye-seo <104261048+joye-seo@users.noreply.github.com>
2023-08-17 15:48:12 +0900

    [FEAT] CommunityData 클래스 추가

---
+++ app/src/main/java/com/example/sixtagram/community/CommunityData.kt
@@ -0,0 +1,9 @@
+package com.example.sixtagram.community
+
+data class CommunityData(
+    val topic : String,
+    val title : String,
+    val content : String,
+    val picture : String,
+    val like : Int,
+)
