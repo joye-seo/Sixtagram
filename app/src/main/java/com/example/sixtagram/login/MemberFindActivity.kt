1b8ad86f15d2d69d81619227b41c2044bd541451
wonjun <dnjswns3026@gmail.com>
2023-08-17 17:36:43 +0900

    [FEAT] : ⚙️ 거주지 입력시 아이디와 비번 찾기 기능 추가 [bug] : 회원 가입 시 비밀번호 최소 자리 이상, 특수 문자, 영문, 숫자를 입력을        안해도 로그인이 안되는 오류 해결,

---
+++ app/src/main/java/com/example/sixtagram/login/MemberFindActivity.kt
@@ -0,0 +1,55 @@
+package com.example.sixtagram.login
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
+import android.widget.Toast
+import com.example.sixtagram.R
+import com.example.sixtagram.memberData.Member
+
+class MemberFindActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_member_find)
+
+        val residence = findViewById<EditText>(R.id.residence)
+        val btn1 = findViewById<Button>(R.id.memberFind)
+        val btn2 = findViewById<Button>(R.id.close)
+        val emailView = findViewById<TextView>(R.id.emailView)
+        val passwordView = findViewById<TextView>(R.id.passwordView)
+
+        val memberList = Member.retrieveMembers()
+        var i: Int
+        btn1.setOnClickListener {
+            if (residence.text.isNullOrEmpty()) {
+                toast("거주지를 입력하여주세요")
+            } else {
+                i = 0
+                for ((index, member) in memberList.withIndex()) {
+                    if (member.residence == residence.text.toString()) {
+                        emailView.text = memberList[index].email
+                        passwordView.text = memberList[index].password
+                        break
+                    }
+                    i++
+                }
+                if (i >= memberList.size){
+                    toast("거주지를 잘못 입력하였습니다.")
+                }
+            }
+        }
+        btn2.setOnClickListener {
+            val homeIntent = Intent(this, LoginActivity::class.java)
+                .putExtra("email2", emailView.text.toString())
+                .putExtra("password2", passwordView.text.toString())
+            setResult(RESULT_OK, homeIntent)
+            finish()
+        }
+    }
+    private fun toast(message: String) {
+        Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
+    }
+}
