866de020432efc978535f9bc95fefeb76c39412f
joye-seo <104261048+joye-seo@users.noreply.github.com>
2023-08-19 03:00:09 +0900

    [FEAT] 좋아요 버튼 클릭 시 하트 애니메이션 작동 구현

--- app/src/main/java/com/example/sixtagram/community/CommunityDetailActivity.kt
+++ app/src/main/java/com/example/sixtagram/community/CommunityDetailActivity.kt
@@ -1,13 +1,14 @@
 package com.example.sixtagram.community
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.widget.ImageView
 import android.widget.LinearLayout
-import android.widget.ListView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.widget.NestedScrollView
+import com.airbnb.lottie.LottieAnimationView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.MultiTransformation
 import com.bumptech.glide.load.resource.bitmap.FitCenter
@@ -23,6 +24,8 @@ class CommunityDetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_community_detail)
 
+        var heart = false
+        var likeCount = 0
         val commentList = Comment.commentList
 
         commentAdapter = CommentAdapter(this, commentList)
@@ -36,17 +39,43 @@ class CommunityDetailActivity : AppCompatActivity() {
         val scroll = findViewById<NestedScrollView>(R.id.community_detail_scroll)
         val container = findViewById<LinearLayout>(R.id.commend_container)
 
+        val btnHeart = findViewById<LinearLayout>(R.id.layout_heart_click)
+
+        btnHeart.setOnClickListener {
+            val lottie = findViewById<LottieAnimationView>(R.id.lottie_heart)
+
+            if (!heart) {
+                //하트 움직이는 애니메이션 작동
+                lottie.playAnimation()
+                lottie.postDelayed({
+                    lottie.pauseAnimation()
+                }, 1000)
+                likeCount++
+                heart = true
+            } else {
+                //하트 빈값으로 만들어줌
+                lottie.frame = 0
+                if (likeCount > 0) {
+                    likeCount--
+                }
+                heart = false
+            }
+
+            Log.d("test123", likeCount.toString())
+
+
+        }
 
         //댓글 관련 구현한 로직 추가적으로 공부해봐야 함!!
-//        commentList.forEach {
-//            val view = LayoutInflater.from(this).inflate(R.layout.item_community_comment, null, false)
-//            val textView = view.findViewById<TextView>(R.id.tv_community_comment_content)
-//            textView.text = it.comment
-//            container.addView(view)
-//        }
+        commentList.forEach {
+            val view = LayoutInflater.from(this).inflate(R.layout.item_community_comment, null, false)
+            val textView = view.findViewById<TextView>(R.id.tv_community_comment_content)
+            textView.text = it.comment
+            container.addView(view)
+        }
 
-        val commentListView = findViewById<ListView>(R.id.listview_comment)
-        commentListView.adapter = commentAdapter
+//        val commentListView = findViewById<ListView>(R.id.listview_comment)
+//        commentListView.adapter = commentAdapter
 
         btnBackDetail.setOnClickListener {
 //            val intent = Intent(this,CommunityActivity::class.java)
