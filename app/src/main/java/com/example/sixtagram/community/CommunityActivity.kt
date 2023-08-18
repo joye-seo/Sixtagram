a82259031993ce5a74aefa0b12d5ae81f2be4aa3
joye-seo <104261048+joye-seo@users.noreply.github.com>
2023-08-18 16:21:45 +0900

    [FEAT] DATA 클래스 별 분리 & Community 댓글 화면 추가

--- app/src/main/java/com/example/sixtagram/community/CommunityActivity.kt
+++ app/src/main/java/com/example/sixtagram/community/CommunityActivity.kt
@@ -1,11 +1,14 @@
 package com.example.sixtagram.community
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.widget.ListView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.sixtagram.R
+import com.example.sixtagram.adapter.CommunityAdapter
 import com.example.sixtagram.calendar.CalendarActivity
+import com.example.sixtagram.data.Community
 import com.example.sixtagram.game.GameStartActivity
 import com.example.sixtagram.member.MemberActivity
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -23,44 +26,13 @@ class CommunityActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_community)
         val btnAdd = findViewById<FloatingActionButton>(R.id.btn_community_add)
+        val communityArrayList = Community.communityArrayList
 
-
+        communityAdapter = CommunityAdapter(this, communityArrayList)
         initNavigation()
 
         val communityListView = findViewById<ListView>(R.id.listview_community)
 
-        val communityArrayList = arrayListOf(
-            CommunityData(
-                "공지사항",
-                "오늘은목요일",
-                "목요일이다..주말언제되냐..ㅠ목요일이다..주말언제되냐..ㅠㅠ목요일이다..주말언제되냐..ㅠㅠ목요일이다..주말언제되냐..ㅠㅠ목요일이다..주말언제되냐..ㅠㅠ목요일이다..주말언제되냐..ㅠㅠ목요일이다..주말언제되냐..ㅠㅠㅠ",
-                "https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F06eb8522-8536-45fe-b868-a619d4fe70c7%2FR0005627.jpg?table=block&id=d70fd7d5-c9b1-4df7-9023-a08f7918bcc0&spaceId=83c75a39-3aba-4ba4-a792-7aefe4b07895&width=2000&userId=8cbc65cd-a833-4515-8c5f-0836cea60f66&cache=v2",
-                3
-            ),
-            CommunityData(
-                "공지사항",
-                "오늘은목요일1",
-                "목요일이다..주말언제되냐..ㅠㅠ",
-                "https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F0438542d-f587-42f1-a207-a4135be33fee%2FKakaoTalk_20230808_091904046.jpg?table=block&id=0e5fa1c5-ba4e-4a7f-b36f-eba35b940a20&spaceId=83c75a39-3aba-4ba4-a792-7aefe4b07895&width=2000&userId=8cbc65cd-a833-4515-8c5f-0836cea60f66&cache=v2",
-                3
-            ),
-            CommunityData(
-                "공지사항",
-                "오늘은목요일2",
-                "목요일이다..주말언제되냐..ㅠㅠ",
-                "https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2Ff13d9219-eec7-4124-b789-396bdbe43179%2FKakaoTalk_20230809_063024964_01.jpg?table=block&id=bf800ccd-274d-4e60-aa4a-162a39224540&spaceId=83c75a39-3aba-4ba4-a792-7aefe4b07895&width=2000&userId=8cbc65cd-a833-4515-8c5f-0836cea60f66&cache=v2",
-                3
-            ),
-            CommunityData(
-                "공지사항",
-                "오늘은목요일3",
-                "목요일이다..주말언제되냐..ㅠㅠ",
-                "https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F0438542d-f587-42f1-a207-a4135be33fee%2FKakaoTalk_20230808_091904046.jpg?table=block&id=0e5fa1c5-ba4e-4a7f-b36f-eba35b940a20&spaceId=83c75a39-3aba-4ba4-a792-7aefe4b07895&width=2000&userId=8cbc65cd-a833-4515-8c5f-0836cea60f66&cache=v2",
-                3
-            ),
-            CommunityData("공지사항", "오늘은목요일4", "목요일이다..주말언제되냐..ㅠㅠ", "", 3),
-            CommunityData("공지사항", "오늘은목요일5", "목요일이다..주말언제되냐..ㅠㅠ", "", 3),
-        )
 
         val adapter = CommunityAdapter(this, communityArrayList)
         communityListView.adapter = adapter
--- app/src/main/java/com/example/sixtagram/community/CommunityDetailActivity.kt
+++ app/src/main/java/com/example/sixtagram/community/CommunityDetailActivity.kt
@@ -1,41 +1,64 @@
 package com.example.sixtagram.community
 
-import android.media.Image
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.ListView
+import android.widget.ScrollView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.widget.NestedScrollView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.MultiTransformation
-import com.bumptech.glide.load.resource.bitmap.CenterCrop
 import com.bumptech.glide.load.resource.bitmap.FitCenter
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.example.sixtagram.R
+import com.example.sixtagram.data.Comment
+import com.example.sixtagram.data.CommunityData
 
 class CommunityDetailActivity : AppCompatActivity() {
 
+    lateinit var commentAdapter: CommentAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_community_detail)
 
+        val commentList = Comment.commentList
+
+        commentAdapter = CommentAdapter(this, commentList)
+
         val btnBackDetail = findViewById<ImageView>(R.id.btn_community_detail_back)
         val btnModifyDetail = findViewById<ImageView>(R.id.btn_community_detail_modify)
         val btnDeleteDetail = findViewById<ImageView>(R.id.btn_community_detail_delete)
-
         val tvTitleDetail = findViewById<TextView>(R.id.tv_community_detail_title)
         val tvContentDetail = findViewById<TextView>(R.id.tv_community_detail_content)
         val imageContentDetail = findViewById<ImageView>(R.id.iv_community_detail_image)
+        val scroll = findViewById<NestedScrollView>(R.id.community_detail_scroll)
+        val container = findViewById<LinearLayout>(R.id.commend_container)
+
+        commentList.forEach {
+           val view =  LayoutInflater.from(this).inflate(R.layout.item_community_comment, null, false)
+            val textView =view.findViewById<TextView>(R.id.tv_community_comment_content)
+           textView.text = it.comment
+            container.addView(view)
+        }
+
+//        val commentListView = findViewById<LinearLayout>(R.id.listview_comment)
+//        commentListView.adapter = commentAdapter
 
         btnBackDetail.setOnClickListener {
 //            val intent = Intent(this,CommunityActivity::class.java)
 //            startActivity(intent)
             finish()
         }
+//        commentListView.isNestedScrollingEnabled = true
 
         val communityData = intent.getSerializableExtra("communityData") as CommunityData
         tvTitleDetail.text = communityData.title
         tvContentDetail.text = communityData.content
-        Glide.with(this).load(communityData.picture).transform(MultiTransformation(FitCenter(),RoundedCorners(10))).into(imageContentDetail)
+        Glide.with(this).load(communityData.picture).transform(MultiTransformation(FitCenter(), RoundedCorners(10)))
+            .into(imageContentDetail)
 
 
     }
