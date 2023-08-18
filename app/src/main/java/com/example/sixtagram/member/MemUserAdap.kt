cc10702f71a0fd1640bd1e99d3b487be05cc0126
Martin Kwon <mindori9097@gmail.com>
2023-08-18 11:53:00 +0900

    [Feature] listView기능 구현 [Modify] activity_member.xml 주석처리부분 삭제 그외 가독성을 위한 코드 줄 바꿈

--- app/src/main/java/com/example/sixtagram/member/MemUserAdap.kt
+++ app/src/main/java/com/example/sixtagram/member/MemUserAdap.kt
@@ -1,7 +1,6 @@
 package com.example.sixtagram.member
 
 import android.content.Context
-import android.text.Layout
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -10,11 +9,10 @@ import android.widget.ImageView
 import android.widget.TextView
 import com.example.sixtagram.R
 
-class MemUserAdap (val context: Context, val UserList: ArrayList<MemUser>) : BaseAdapter()
-
-{
+class MemUserAdap(val context: Context, val UserList: ArrayList<MemUser>) : BaseAdapter() {
     override fun getView(position: Int, convertView: View?, parent: ViewGroup?): View {
-        val view: View = LayoutInflater.from(context).inflate(R.layout.activity_memuseractivity, null)
+        val view: View =
+            LayoutInflater.from(context).inflate(R.layout.activity_memuseractivity, null)
 
         val profile = view.findViewById<ImageView>(R.id.iv_profile_kms)
         val name = view.findViewById<TextView>(R.id.tv_name)
