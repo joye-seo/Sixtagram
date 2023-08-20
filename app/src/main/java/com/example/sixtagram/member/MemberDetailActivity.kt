2c7bcc79b263b7d4d4ea5cb5edde3787fd666ae2
Martin Kwon <mindori9097@gmail.com>
2023-08-20 10:38:13 +0900

    [Enhancement] MemberDetailActivity.kt 에서 상단에 멤버별 프로필 사진과 이름 반영

--- app/src/main/java/com/example/sixtagram/member/MemberDetailActivity.kt
+++ app/src/main/java/com/example/sixtagram/member/MemberDetailActivity.kt
@@ -10,6 +10,10 @@ import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.MultiTransformation
+import com.bumptech.glide.load.resource.bitmap.FitCenter
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.example.sixtagram.R
 import com.example.sixtagram.memberData.MemberData
 
@@ -64,6 +68,8 @@ class MemberDetailActivity : AppCompatActivity(), MemDelDialogInterface {
         val memberBlog = findViewById<TextView>(R.id.tv_blog)
         val memberGithub = findViewById<TextView>(R.id.tv_github)
         val memberComment = findViewById<TextView>(R.id.tv_saying)
+        val memberImage = findViewById<ImageView>(R.id.imageView2)
+        val memberName = findViewById<TextView>(R.id.textView9)
 
         val intent = intent
         val selectedUser = intent.getSerializableExtra("MemberData") as? MemberData
@@ -77,6 +83,10 @@ class MemberDetailActivity : AppCompatActivity(), MemDelDialogInterface {
             memberBlog.text = selectedUser.blog
             memberGithub.text = selectedUser.github
             memberComment.text = selectedUser.text
+            memberName.text = selectedUser.name
+            Glide.with(this)
+                .load(selectedUser.imageEdit) .transform(MultiTransformation(FitCenter(), RoundedCorners(500)))
+                .into(memberImage)
         } else {
             // 선택한 사용자 정보가 없을 때의 처리
         }
