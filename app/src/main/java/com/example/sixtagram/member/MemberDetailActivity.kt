e597ed993e8dc4f86151b7b160de62c235ce47bc
Martin Kwon <mindori9097@gmail.com>
2023-08-20 10:38:23 +0900

    [Enhancement] MemberDetailActivity.kt 에서 상단에 멤버별 프로필 사진과 이름 반영

--- app/src/main/java/com/example/sixtagram/member/MemberDetailActivity.kt
+++ app/src/main/java/com/example/sixtagram/member/MemberDetailActivity.kt
@@ -3,11 +3,8 @@ package com.example.sixtagram.member
 import android.content.Intent
 import android.net.Uri
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
 import androidx.activity.result.ActivityResultLauncher
 import androidx.appcompat.app.AppCompatActivity
 import com.bumptech.glide.Glide
@@ -25,8 +22,6 @@ class MemberDetailActivity : AppCompatActivity(), MemDelDialogInterface {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_member_detail)
-
-
 
 
         val imgMemBack = findViewById<ImageView>(R.id.btn_mem_back)
@@ -85,7 +80,8 @@ class MemberDetailActivity : AppCompatActivity(), MemDelDialogInterface {
             memberComment.text = selectedUser.text
             memberName.text = selectedUser.name
             Glide.with(this)
-                .load(selectedUser.imageEdit) .transform(MultiTransformation(FitCenter(), RoundedCorners(500)))
+                .load(selectedUser.imageEdit)
+                .transform(MultiTransformation(FitCenter(), RoundedCorners(500)))
                 .into(memberImage)
         } else {
             // 선택한 사용자 정보가 없을 때의 처리
